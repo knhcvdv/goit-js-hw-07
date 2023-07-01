78cd67c2f9bc7eb011e5cb3439971cd800f76f74
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-07-02 01:21:50 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgListRef = document.querySelector('.gallery');
+const previewPhoto = createPreviewPhoto(galleryItems);
+
+
+imgListRef.insertAdjacentHTML('beforeend', previewPhoto);
+    
+
+function createPreviewPhoto(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`}).join('');
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '300',
+});
 
 console.log(galleryItems);
