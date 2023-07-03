77c2acab7ef86f97dac75a2ca2884286dc3349c5
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-07-03 18:01:42 +0300

    ex-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,10 +10,12 @@ imgListRef.insertAdjacentHTML('beforeend', previewPhoto);
 function createPreviewPhoto(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `<li class="gallery__item">
-   <a class="gallery__link" href=${original}>
-      <img class="gallery__image" src=${preview} alt=${description} />
-   </a>
-</li>`}).join('');
+         <a class="gallery__link" href=${original}>
+            <img class="gallery__image" src=${preview} alt=${description} />
+        </a>
+    </li>`
+    })
+    .join('');
 }
 
 
